71846ccba60009789f5d1a450009ce8d705d5a6e
Kyle Bustard <bustardkyle@gmail.com>
2018-05-18 21:06:24 -0500

    install and configure webpack devserver

---
+++ config/webpack.dev.js
@@ -0,0 +1,16 @@
+const path = require("path")
+
+module.exports = {
+  entry: {
+    main: "./src/main.js"
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "dist"
+  }
+}
