c92ca5943f316e29aa695a747ceb4ff578207d0d
Kyle Bustard <bustardkyle@gmail.com>
2018-05-22 20:22:09 -0500

    add react.js

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <h1 className="test">Hello from React!</h1>, 
+  document.getElementById("react-root")
+)
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 require("babel-runtime/regenerator")
+require("babel-register")
 require("webpack-hot-middleware/client?reload=true")
 require("./main.css")
 require("./index.html")
-debugger
+require("./app.js")
